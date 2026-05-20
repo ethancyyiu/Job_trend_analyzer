6e50cf37384f79635cd54e8838f2892682f3a210
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-20 12:18:08 -0700

    created function to save posting unless there is a conflict of same page

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,3 +4,11 @@ from datetime import date
 
 DB = psycopg2.connect("postgresql://localhost/jobtrends")
 
+def save(posting):
+    with DB.cursor() as cur:
+        cur.execute("""
+            INSERT INTO postings (title, company, location, date_scraped)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT DO NOTHING
+        """, (posting['title'], posting['company'], posting['location'], date.today()))
+    DB.commit()

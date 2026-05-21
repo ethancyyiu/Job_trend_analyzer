70cb45c133efeaa587adf7684669e62032112572
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-21 15:38:30 -0700

    added the skills I can think of

--- analysis/skill_extractor.py
+++ analysis/skill_extractor.py
@@ -4,3 +4,16 @@ import os
 load_dotenv()
 
 DB = psycopg2.connect(os.environ["DATABASE_URL"])
+
+SKILLS = [
+    "python", "javascript", "typescript", "java", "c++", "c#", "go", "rust", "ruby", "scala", "c"
+
+    "react", "vue", "angular", "node.js", "fastapi", "django", "flask",
+
+    "sql", "postgresql", "mysql", "mongodb", "redis",
+    "pandas", "numpy", "scikit-learn", "tensorflow", "pytorch",
+
+    "aws", "gcp", "azure", "docker", "kubernetes", "terraform",
+
+    "git", "github", "linux", "rest api", "graphql", "kafka", "fastapi"
+]

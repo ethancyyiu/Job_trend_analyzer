ea419ef7e90184ac9cb44fa1daeefc239e8c2cdd
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-20 22:45:18 -0700

    added the table schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE postings (
+    id          SERIAL PRIMARY KEY,
+    title       TEXT,
+    company     TEXT,
+    location    TEXT,
+    skills      TEXT[],          -- fill later with extractor
+    date_scraped DATE,
+    UNIQUE (title, company, date_scraped)
+);

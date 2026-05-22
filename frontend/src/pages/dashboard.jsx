aee7fdebff4489241c6606a1935d3a9aa81ce9cf
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-22 11:42:36 -0700

    added a way to update the data to the website

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -2,4 +2,16 @@ import {useEffect, useState} from "react"
 
 export function dashboard() {
     const [data, setData] = useState([])
+
+    useEffect(
+        function() {
+            axios.get("http://localhost:8000/trends").then(
+                function(answer) {
+                    return setData(answer)})}, [])
+
+    return <div>
+        <h2>Postings Over Time</h2>
+        
+    </div>
 }
+

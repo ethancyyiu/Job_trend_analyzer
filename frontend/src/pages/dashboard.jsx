6c32c57148388f2bb67ab028a09299b8bf2c4593
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-22 12:21:55 -0700

    added the skills_view page and fixed some bugs in dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -11,15 +11,17 @@ export function dashboard() {
                 function(answer) {
                     return setData(answer)})}, [])
 
-    return <div>
-        <h2>Postings Over Time</h2>
-        <LineChart width = {800} height = {400} data = {data}>
-            <XAxis dateKey = "date" />
-            <YAxis />
-            <Line type = "monotone" dataKey = "count" stroke = "#8884d8" />
-            <CartesianGrid strokeDasharray = "3 3" />
-            <ToolTip />
-        </LineChart>
-    </div>
+    return (
+        <div>
+            <h2>Postings Over Time</h2>
+            <LineChart width = {800} height = {400} data = {data}>
+                <XAxis dateKey = "date" />
+                <YAxis />
+                <Line type = "monotone" dataKey = "count" stroke = "#8884d8" />
+                <CartesianGrid strokeDasharray = "3 3" />
+                <ToolTip />
+            </LineChart>
+        </div>
+    )
 }
 
---
+++ frontend/src/pages/skills_view.jsx
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react"
+import axios from "axios"
+import {Bar, BarChart, XAxis, YAxis, ToolTip, CartesianGrid} from "recharts"
+
+export function dashboard() {
+    const [data, setData] = useState([])
+
+    useEffect(
+        function() {
+            axios.get("http://localhost:8000/trends").then(
+                function(answer) {
+                    return setData(answer)})}, [])
+
+    return (
+        <div>
+            <h2>Top Skills In Demand</h2>
+            <BarChart width = {800} height = {400} data = {data}>
+                <XAxis dateKey = "date" />
+                <YAxis />
+                <ToolTip />
+                <Bar dataKey="count" fill="#82ca9d" />
+            </BarChart>
+        </div>
+    )
+}
+

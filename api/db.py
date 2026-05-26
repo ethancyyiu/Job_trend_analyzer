0e2e04f1c3aede1a97454247593f702ba326326e
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-25 19:06:49 -0700

    added connection pool so I do not need to open connection everytime I query

--- api/db.py
+++ api/db.py
@@ -4,9 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB = psycopg2.connect(os.environ["DATABASE_URL"])
+pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dsn=os.environ["DATABASE_URL"]
+)
 
 def query(sql, params=None):
-    with DB.cursor() as cur:
-        cur.execute(sql, params)
-        return cur.fetchall()
+    conn = pool.getconn()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(sql, params)
+            return cur.fetchall()
+    finally:
+        pool.putconn(conn)

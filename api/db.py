67f30ebcd802875e35daee2bcaa0b08f1f47b26d
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-05-26 18:26:20 -0700

    fixed more bugs

--- api/db.py
+++ api/db.py
@@ -5,17 +5,26 @@ from psycopg2 import pool
 
 load_dotenv()
 
-pool = psycopg2.pool.SimpleConnectionPool(
-    minconn=1,
-    maxconn=10,
-    dsn=os.environ["DATABASE_URL"]
-)
+# Lazily create the connection pool so module import doesn't fail
+_pool = None
+
+def _get_pool():
+    global _pool
+    if _pool is None:
+        dsn = os.environ.get("DATABASE_URL")
+        if not dsn:
+            raise RuntimeError("DATABASE_URL environment variable is not set")
+        _pool = psycopg2.pool.SimpleConnectionPool(minconn=1, maxconn=10, dsn=dsn)
+    return _pool
 
 def query(sql, params=None):
-    conn = pool.getconn()
+    p = _get_pool()
+    conn = None
     try:
+        conn = p.getconn()
         with conn.cursor() as cur:
             cur.execute(sql, params)
             return cur.fetchall()
     finally:
-        pool.putconn(conn)
+        if conn is not None:
+            p.putconn(conn)

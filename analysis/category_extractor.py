4740e09fc2e69d05d92e903b532652693b24d306
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-06-17 00:48:55 -0400

    tried to fix graph starting at 0, unsuccessful

--- analysis/category_extractor.py
+++ analysis/category_extractor.py
@@ -72,7 +72,7 @@ def category_extractor():
                 category = "data analyst" 
                 
             else:
-                time.sleep(3)
+                time.sleep(3.5)
                 category = get_gemini(title)
 
             print(category)
